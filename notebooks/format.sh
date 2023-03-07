49b39e51c23f0cc8791a5d5395b2d097a99bdfa3
Michelle Weirathmueller <mweirathmueller@jasco.com>
2023-03-06 21:28:15 -0500

    minor update to formatting script

--- notebooks/format.sh
+++ notebooks/format.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Make sure this script is executable using chmod +x format.sh
-# Then just run it with the following command line args:
+# Then just run it with the following command line args, for example:
+# ./format.sh "glucose-part06.ipynb" "Post 6: bla bla" "2023-03-06-data_part06.html"
+
 nb=$1 # jupyter notebook name
 title=$2 # post title
 filename=$3 # new file name
